586e51173fb5d2b8c92ed4d40f0b7b76f14401ed
MohRaouf <moh.proff@gmail.com>
2021-03-21 21:00:18 +0200

    refine some object structures

--- models/book.js
+++ models/book.js
@@ -3,19 +3,18 @@ const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
     //_id Auto increment [not finshed = > seaarch ]
-    name  : { type : String, required : true, index : true },
-    photo : { data : Buffer,  contentType : String },
-    description : { type : String, required : true, },
-    reviews    : [{ type: mongoose.Schema.Types.ObjectId, ref: 'review'},],
-    authorId   : {  type: mongoose.Schema.Types.ObjectId, ref: 'author'},
-    categoryId : [{ type: mongoose.Schema.Types.ObjectId, ref: 'category'}],
-    avgRating  : {}, //calculated
+    name: { type: String, required: true, index: true },
+    photo: { data: Buffer, contentType: String },
+    description: { type: String, required: true },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'review' }],
+    authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'author', require: true },
+    categoryId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'category', require: true }],
+    avgRating: {}, //calculated
     ratingCount: {}, //calculated
 })
 
 //creating book model to use it in validation with a middleware
-const BookModel = mongoose.model('book',bookSchema)
+const BookModel = mongoose.model('book', bookSchema)
 
 //exporting the book model 
 module.export = BookModel
-
