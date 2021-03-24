1f4c7f160cd446b28645b53807cdd297f8a14767
rjOwl <mostafayossef100@yahoo.com>
2021-03-24 17:59:27 +0200

    Setup atlas database

--- boot/dbConnecion.js
+++ boot/dbConnecion.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose')
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/goodbooks'
-mongoose.connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}, (err) => {
-    if (err) console.error(err)
-    else console.log("Connected To DB")
-})
+const uri = "mongodb+srv://team7-ninjas:123intake41@goodbookscluster.mrby6.mongodb.net/awesome-reads?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},
+  err => {
+    if(err) console.error(err)
+    else console.log("Connected To Atlas Datebase")
+  }
+)
+
+
+// URL = "mongodb+srv://team7-ninjas:123intake41@goodbookscluster.mrby6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://team7-ninjas:123intake41@goodbookscluster.mrby6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
