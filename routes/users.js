a3d5523a77e48bf8f63adf3ce70256e5192a5ddc
rjOwl <mostafayossef100@yahoo.com>
2021-04-07 22:14:23 +0200

    Added reviews endpoint

--- routes/users.js
+++ routes/users.js
@@ -167,6 +167,57 @@ userRouter.delete("/remove_book", jwtHelpers.verifyAccessToken,async(req, res)=>
     }
 })
 
+const addReviewToReviews = async (res, userid, bookid, reviewBody)=>{
+    try{
+       result = await ReviewModel.create({
+            userId: mongoose.Types.ObjectId(userid),
+            bookId: mongoose.Types.ObjectId(bookid),
+            body: reviewBody
+        })
+        .then((doc)=>{return doc})
+        .catch((err)=>{res.sendStatus(424); console.log("[X] [await catch addReviewToReviews\]:\n====================\n"); return -1})
+        return result
+    }catch(exception){
+        console.log("[X] [await catch addReviewToReviews\]:\n====================\n",exception);        res.sendStatus(503)
+        return -1
+    }
+}
+
+const addReviewToBook = async (res, bookid, reviewId)=>{
+    try{
+       result = await BookModel.findOneAndUpdate({
+            _id: mongoose.Types.ObjectId(bookid),
+        },{
+            $push:{
+                reviews: mongoose.Types.ObjectId(reviewId)
+            }
+        })
+        .then((doc)=>{return doc})
+        .catch((err)=>{res.sendStatus(424); console.log("[X] [await catch addReviewToBook\]:\n====================\n"); return -1})
+        return result
+    }catch(exception){
+        console.log("[X] [await catch addReviewToBook\]:\n====================\n",exception);        res.sendStatus(503)
+        res.sendStatus(503)
+        return -1
+    }
+}
+
+userRouter.post("/add_review", async (req, res) => {
+    const userId = req.body.userId;
+    const bookId = req.body.bookId;
+    const body =  req.body.body;
+    reviewDoc= await addReviewToReviews(res, userId, bookId, body)
+    if(reviewDoc != -1){
+        console.log("======================= 1 ============================")
+        console.log(reviewDoc)
+        addToBook = await addReviewToBook(res, bookId, reviewDoc._id)
+        if(addToBook != -1){
+            console.log(addToBook)
+            res.sendStatus(200)
+        }
+    }
+})
+
 
 userRouter.delete("/remove_book_old", async (req, res) => {
     const reqUsername = req.body.username;
