2d6d3f625e346f53802f5a0240caaf3a164b26e1
MohRaouf <moh.proff@gmail.com>
2021-04-16 23:30:44 +0200

    Fix some async-await and promises use

--- routes/authors.js
+++ routes/authors.js
@@ -4,47 +4,37 @@ const jwtHelpers = require('../helpers/jwt_helper')
 const authorRouter = express.Router();
 
 /* Get All Authors no need for Authentication */
-authorRouter.get("/", async(req, res) => {
-    const allAuthors = await AuthorModel.find()
-        .catch((err) => {
-            console.error(err)
-            return res.status(400).send("Bad Request")
-        })
-    // console.log("All Authors : ", allAuthors)
-    return res.json(allAuthors);
+authorRouter.get("/", (req, res) => {
+    AuthorModel.find().then((allAuthors) => {
+        if (allAuthors) { return res.json(allAuthors) }
+        else { return res.status(404).end() }
+    }).catch((err) => { return res.status(400).end() })
 })
 //when request to get popular author
 // >>> with query
-authorRouter.get("/top",async (req,res)=>{
-        // const topAuthors=await AuthorModel.getTopAuthors(request.query.size);
-        const topAuthors=await AuthorModel.getTopAuthors(1)
-        .catch((err)=>{
-            console.log(err);
-            return res.status(500).send("Internal Server Error")
+authorRouter.get("/top", async (req, res) => {
+    AuthorModel.getTopAuthors(1)
+        .then((topAuthors) => {
+            if (topAuthors) { return res.json(topAuthors) }
+            else { return res.status(404).end() }
         })
-        if(topAuthors){return res.json(topAuthors)}
-        else {return res.status(404).send("Not Found")}
+        .catch((err) => { return res.status(500).end() })
 })
 
 /* Get Author by ID no need for Authentication */
-authorRouter.get("/:author_id", async(req, res) => {
+authorRouter.get("/:author_id", (req, res) => {
     const id = req.params.author_id;
-    console.log(id)
-    const author = await AuthorModel.findById(id).populate({ path: 'books', select: '_id name photo'}).exec()
-        .catch((err) => {
-            console.log(err)
-            return res.status(500).send('Internal server error')
+    AuthorModel.findById(id).populate({ path: 'books', select: '_id name photo' })
+        .then((author) => {
+            if (author) return res.json(author);
+            return res.status(404).end()
+        }).catch((err) => {
+            return res.status(500).end()
         })
-    if (author) {
-        // console.log(author)
-        return res.json(author);
-    }
-    console.log('Not Found')
-    return res.status(404).send("Not Found")
 })
 
 /* Insert new Author need Authentication */
-authorRouter.post("/", jwtHelpers.verifyAccessToken,jwtHelpers.isAdmin, async(req, res) => {
+authorRouter.post("/", jwtHelpers.verifyAccessToken, jwtHelpers.isAdmin, (req, res) => {
     const authorInfo = {
         fname: req.body.fname,
         lname: req.body.lname, //optional
@@ -52,25 +42,14 @@ authorRouter.post("/", jwtHelpers.verifyAccessToken,jwtHelpers.isAdmin, async(re
         ...(req.body.dob ? { dob: req.body.dob } : {}), //optional
         ...(req.body.gender ? { gender: req.body.gender } : {}), //optional
     }
-    await AuthorModel.create(authorInfo).then((author) => {
-
-        console.log(`Author ${authorInfo.fname} ${authorInfo.lname} Added Successfully`)
-        return res.status(201).send("Created")
-
-    }).catch((err) => {
-
-        console.error(err);
-        return res.status(500).send("Failed To Add New Author")
-
-    })
+    AuthorModel.create(authorInfo).then((author) => {
+        return res.status(201).end()
+    }).catch((err) => { return res.status(500).end() })
 })
 
 /* Update Author with ID need Authentication */
-authorRouter.patch("/:author_id", jwtHelpers.verifyAccessToken,jwtHelpers.isAdmin, async(req, res) => {
-
+authorRouter.patch("/:author_id", jwtHelpers.verifyAccessToken, jwtHelpers.isAdmin, (req, res) => {
     const id = req.params.author_id;
-    console.log(`Updating Book ID : ${id}`)
-
     const newAuthorInfo = {
         ...(req.body.fname ? { fname: req.body.fname } : {}),
         ...(req.body.lname ? { lname: req.body.lname } : {}),
@@ -78,34 +57,21 @@ authorRouter.patch("/:author_id", jwtHelpers.verifyAccessToken,jwtHelpers.isAdmi
         ...(req.body.dob ? { dob: req.body.dob } : {}),
         ...(req.body.gender ? { gender: req.body.gender } : {}),
     }
-    // console.log(`Updated Info : ${newAuthorInfo}`)
-    const updatedDoc = await AuthorModel.findByIdAndUpdate({ _id: id }, newAuthorInfo, { new: true, useFindAndModify: false }).
-    catch((err) => {
-        console.error("====Error===>", err)
-        return res.status(400).send("Bad Request")
-    })
-    if (updatedDoc) {
-        // console.log(`Updated Info : ${updatedDoc}`)
-        return res.status(202).send("Accepted")
-    }
-    // console.log(`Updated Info : ${updatedDoc}`)
-    return res.status(404).send("author not found")
+    AuthorModel.findByIdAndUpdate({ _id: id }, newAuthorInfo, { new: true, useFindAndModify: false })
+        .then((updatedDoc) => {
+            if (updatedDoc) return res.status(202).end()
+            return res.status(404).end()
+        }).catch((err) => { return res.status(400).end() })
 })
 
 /* Delete Author with ID need Authentication */
-authorRouter.delete("/:author_id", jwtHelpers.verifyAccessToken,jwtHelpers.isAdmin, async(req, res) => {
+authorRouter.delete("/:author_id", jwtHelpers.verifyAccessToken, jwtHelpers.isAdmin, (req, res) => {
     const id = req.params.author_id;
-    const docToDelete = await AuthorModel.deleteOne({ _id: id }, { new: true, useFindAndModify: false })
-        .catch((err) => {
-            console.error(err)
-            return res.status(500).send("Internal server error")
-        })
-    if (docToDelete) {
-        console.log(`Author ID : ${id} Deleted`)
-        return res.status(200).send("Deleted")
-    }
-    console.log(`Author Not Found`)
-    return res.status(404).send("Book not found")
-})
+    AuthorModel.deleteOne({ _id: id }, { new: true, useFindAndModify: false })
+        .then((deletedDoc) => {
+            if (deletedDoc) return res.status(200).end()
+            return res.status(404).end()
+        }).catch((err) => { return res.status(500).end() })
 
+})
 module.exports = authorRouter;
